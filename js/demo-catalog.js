0cf7965fef34af58fb727379107cb5466b0a50b9
wdsanchezc <wdsanchezc@gmail.com>
2026-09-11 23:00:35 -0600

    Add demo music catalog

---
+++ js/demo-catalog.js
@@ -0,0 +1,26 @@
+export const DEMO_GENRES = [
+  {id:"pop",name:"Pop",icon:"✨"},
+  {id:"urbano",name:"Urbano / Reggaetón",icon:"🔥"},
+  {id:"baladas",name:"Baladas",icon:"❤️"},
+  {id:"rock",name:"Rock",icon:"🎸"},
+  {id:"latino",name:"Latino",icon:"🌴"},
+  {id:"party",name:"Fiesta",icon:"🎉"}
+];
+
+export const DEMO_ARTISTS = [
+  {id:"queen",name:"Queen",image_url:""},
+  {id:"michael-jackson",name:"Michael Jackson",image_url:""},
+  {id:"luis-fonsi",name:"Luis Fonsi",image_url:""},
+  {id:"bruno-mars",name:"Bruno Mars",image_url:""},
+  {id:"katy-perry",name:"Katy Perry",image_url:""},
+  {id:"rick-astley",name:"Rick Astley",image_url:""}
+];
+
+export const DEMO_SONGS = [
+  {id:"demo-1",provider_track_id:"kJQP7kiw5Fk",title:"Despacito",artist:"Luis Fonsi ft. Daddy Yankee",artist_id:"luis-fonsi",genre_id:"latino",thumbnail_url:"https://i.ytimg.com/vi/kJQP7kiw5Fk/hqdefault.jpg",duration_seconds:282,is_featured:true,is_honduras_hit:true,play_count:118},
+  {id:"demo-2",provider_track_id:"fJ9rUzIMcZQ",title:"Bohemian Rhapsody",artist:"Queen",artist_id:"queen",genre_id:"rock",thumbnail_url:"https://i.ytimg.com/vi/fJ9rUzIMcZQ/hqdefault.jpg",duration_seconds:354,is_featured:true,is_honduras_hit:false,play_count:79},
+  {id:"demo-3",provider_track_id:"Zi_XLOBDo_Y",title:"Billie Jean",artist:"Michael Jackson",artist_id:"michael-jackson",genre_id:"pop",thumbnail_url:"https://i.ytimg.com/vi/Zi_XLOBDo_Y/hqdefault.jpg",duration_seconds:294,is_featured:true,is_honduras_hit:true,play_count:92},
+  {id:"demo-4",provider_track_id:"OPf0YbXqDm0",title:"Uptown Funk",artist:"Mark Ronson ft. Bruno Mars",artist_id:"bruno-mars",genre_id:"party",thumbnail_url:"https://i.ytimg.com/vi/OPf0YbXqDm0/hqdefault.jpg",duration_seconds:270,is_featured:true,is_honduras_hit:true,play_count:141},
+  {id:"demo-5",provider_track_id:"CevxZvSJLk8",title:"Roar",artist:"Katy Perry",artist_id:"katy-perry",genre_id:"pop",thumbnail_url:"https://i.ytimg.com/vi/CevxZvSJLk8/hqdefault.jpg",duration_seconds:270,is_featured:false,is_honduras_hit:false,play_count:66},
+  {id:"demo-6",provider_track_id:"dQw4w9WgXcQ",title:"Never Gonna Give You Up",artist:"Rick Astley",artist_id:"rick-astley",genre_id:"pop",thumbnail_url:"https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",duration_seconds:213,is_featured:false,is_honduras_hit:false,play_count:51}
+];
