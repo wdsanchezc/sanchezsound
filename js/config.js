494fc7ae7a204c4d369637c8fcd94ad0655763d3
wdsanchezc <wdsanchezc@gmail.com>
2026-09-11 23:00:18 -0600

    Add Supabase configuration placeholder

---
+++ js/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  SUPABASE_URL: "",
+  SUPABASE_PUBLISHABLE_KEY: "",
+  YOUTUBE_FUNCTION_NAME: "youtube-search"
+};
+
+export const APP = {
+  name: "SanchezSound",
+  subtitle: "Digital Jukebox",
+  defaultRoomName: "SanchezSound Party"
+};
